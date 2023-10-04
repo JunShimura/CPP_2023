f8c3188ee9ba0ce2c4f4b70a3e41f05f800679fb
Jun Shimura <junshimura@gmail.com>
2023-10-04 17:34:36 +0900

    1st

---
+++ CSharpSample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Csharp");
+            int[] x = { 10, 2, 300, -4 };
+            for (int i = 0; i <= 4; i++)
+            {
+                //cout << "x[" << i << "]=" << x[i] << endl;
+                Console.WriteLine($"x[{i}]={x[i]}");
+            }
+        }
+    }
+}
---
+++ Chapter3/03-variable.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x[4] = { 10,2,300,-4 };
+	for (int i = 0; i <= 10000; i++) {
+		cout << "x[" << i << "]=" << x[i] << endl;
+	}
+	
+	std::string s = "‚µ‚Þ‚ç";
+	cout <<s<<
+		sizeof(int) << "," <<
+		numeric_limits<int>::lowest() << "," <<
+		numeric_limits<int>::max() << endl;
+	cout <<
+		sizeof(long) << "," <<
+		numeric_limits<long>::lowest() << "," <<
+		numeric_limits<long>::max() << endl;
+	cout <<
+		sizeof(float) << "," <<
+		numeric_limits<float>::lowest() << "," <<
+		numeric_limits<float>::max() << endl;
+	cout <<
+		sizeof(double) << "," <<
+		numeric_limits<double>::lowest() << "," <<
+		numeric_limits<double>::max() << endl;
+
+}
