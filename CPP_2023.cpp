4f84dc3b54fdd88da99c30a16f9844b14de8b75f
Jun Shimura <junshimura@gmail.com>
2023-10-04 17:31:49 +0900

    プロジェクト ファイルを追加します。

---
+++ CPP_2023.cpp
@@ -0,0 +1,9 @@
+﻿// CPP_2023.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello\n World!\nby しむら" << endl<< "きょうもごはんがおいしいぞ";
+}
