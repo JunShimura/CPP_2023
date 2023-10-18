1bac58b4d13265572e2fee97c9097528353fdca9
Jun Shimura <junshimura@gmail.com>
2023-10-18 15:50:43 +0900

    cpp_ex01

---
+++ cpp_ex1/cpp_ex1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int crane;  //鶴
+	int turtle; //亀
+	int heads; //頭
+	int legs; //脚
+	
+	cout << "頭の数は：";		//Console.Write("頭の数は：");
+	/* 入力エラーの処理
+	while (true) {
+		cout << "頭の数は：";		//Console.Write("頭の数は：");
+		string s;
+		cin >> s;
+		try {
+			heads = stoi(s);
+		}
+		catch (invalid_argument& e) {
+			cout << "入力エラー";
+			continue;
+		}
+		break;
+	}
+	*/
+	cin >> heads;				//heads = int.Parse(Console.ReadLine());
+	cout << "脚の数は：";		//Console.Write("脚の数は：");
+	cin >> legs;				//legs = int.Parse(Console.ReadLine());
+
+	//ここで鶴と亀の数をcraneとturtleに代入
+	turtle = (legs / 2) - heads;
+	crane = heads - turtle;
+
+	cout << "鶴は" << crane << "、亀は" << turtle;	//Console.WriteLine("鶴は{0}、亀は{1}", crane, turtle);
+	//printf("鶴は%d、亀は%d", crane, turtle);
+
+}
+
